9c773afacf74e16fd46df0f16dbb7beddd6afd9a
James <boydston.j@gmail.com>
2017-07-24 19:07:53 -0700

    cleanup

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ const moment = require('moment');
 moment().format();
 const chalk = require('chalk');
 
-// It is blue(dayWeek, month day year, hh:mm:ss tod) .
-// It is magenta(count) day of the year.
-// It is cyan(count) seconds into the day.
-// It green(is) during Daylight Savings Time.
-// It red(is/is not) a leap year.
-
-let now = moment()
-
-
 console.log(`It is ${chalk.blue(moment().format("LLLL"))}.`);
 console.log(`It is ${chalk.magenta(moment().format("DDDo"))} day of the year.`);
 
@@ -23,11 +14,6 @@ function seconds() {
   return secCount
 }
 console.log(`It is ${chalk.cyan(seconds())} seconds into the day.`);
-
-
-
-
-
 
 function dst() {
   if (moment().isDST()) {
